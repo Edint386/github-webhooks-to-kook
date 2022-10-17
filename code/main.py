d2972391c6492b97b36330a75430885b9bda3c0e
Ewait <s2522446388@163.com>
2022-10-17 18:33:30 +0800

    docs(main): move main.py to ./code , set all log files to ./code/log

--- code/main.py
+++ code/main.py
@@ -1,8 +1,6 @@
 import asyncio
 import hmac
-
 import aiofiles
-from khl import *
 import aiohttp
 import copy
 import io
@@ -10,18 +8,20 @@ import json
 from PIL import Image, ImageDraw, ImageFont, ImageStat, ImageFilter, ImageEnhance
 from aiohttp import web, FormData
 from aiohttp.web_request import Request
+from khl import Bot,Message
+from khl.card import Element,Card,CardMessage,Module,Types
 from khl.command import Rule
 
-from card_ui import *
+from card_ui import icon,ui
 repo_secret = {}
 ping_temp = {}
 com_temp = {}
-
 guild_setting = {}
 routes = web.RouteTableDef()
-
-bot = Bot(token='xxxxxxxxxxxxxxxxxx')
-f.close()
+# 初始化bot
+with open('./config/config.json', 'r', encoding='utf-8') as f:
+    config = json.load(f)
+bot = Bot(token=config['token'])
 
 # 读取文件
 async def read_file(path) -> dict:
@@ -31,12 +31,12 @@ async def read_file(path) -> dict:
 # 写入文件
 async def write_file(path, value):
     async with aiofiles.open(path, 'w',encoding='utf-8') as f:
-        await f.write(json.dumps(value,ensure_ascii=False))
+        await f.write(json.dumps(value,ensure_ascii=False,indent=2, sort_keys=True,))
 
-path_repo = 'temp\\repo_setting.txt'
-path_guild = 'temp\\guild_setting.txt'
-path_did = 'temp\\did_temp.txt'
-path_secret = 'temp\\secret.txt'
+path_repo = './log/repo_setting.json'
+path_guild = './log/guild_setting.json'
+path_did = './log/did_temp.json'
+path_secret = './log/secret.json'
 @bot.task.add_date()
 async def tgrds():
     global res_setting
@@ -49,10 +49,6 @@ async def tgrds():
     repo_secret = await read_file(path_secret)
 
 
-
-
-
-
 @bot.task.add_interval(seconds=60)
 async def save_Data():
     await write_file(path_repo,res_setting)
@@ -60,11 +56,15 @@ async def save_Data():
     await write_file(path_did,ping_temp)
     await write_file(path_secret,repo_secret)
 
-
-
+# 基本请求，用于验证是否在线且能正常访问
+@routes.get('/')
+async def link_test(request:web.get):
+    print("[request] /")
+    return web.Response(body="Hello", status=200)
 
 @routes.post('/hook')
 async def github_webhook(request: web.Request):
+    print("[request] /hook")
     type = request.headers['X-GitHub-Event']
     did = request.headers['X-GitHub-Delivery']
     if 'X-HUB-SIGNATURE' in request.headers:
@@ -78,7 +78,7 @@ async def github_webhook(request: web.Request):
     rid = str(data["repository"]["id"])
     ping_temp[did] = {'rid': rid, 'secret': secret_state, 'sign': sign,'body':data}
     if type == 'ping':
-        print('ping')
+        print(f"[Ping]")
         return web.Response(body="Pong!", status=200)
 
     repo_name = data["repository"]["full_name"]
@@ -91,30 +91,27 @@ async def github_webhook(request: web.Request):
     compare = data['compare']
     message :str= data["commits"][0]["message"]
 
+    # 如果在setting里面，代表已经ping过了
     if rid in res_setting:
-
         c = Card(color=ui.default_color)
         c.append(Module.Header(f'New {type} Event'))
-        c.append(Module.Section(Element.Text(f'> [{sender_name}]({sender_url}) \n[{repo_name}]({repo_url})'),
+        c.append(Module.Section(Element.Text(f'> user: [{sender_name}]({sender_url}) \nrepo: [{repo_name}]({repo_url})',Types.Text.KMD),
                                 Element.Image(sender_avatar), mode=Types.SectionMode.LEFT))
         c.append(Module.Divider())
         if bhash != '':
-            c.append(Module.Context(f'> Hash:\n[{bhash} -> {ahash}]({compare})'))
+            c.append(Module.Context(Element.Text(f'> Hash:\n[{bhash} -> {ahash}]({compare})',Types.Text.KMD)))
         print(message)
         while message.find('\n\n') >0:
             message = message.replace('\n\n','\n')
-        c.append(Module.Context(f'> Message:\n**{message}**'))
+        c.append(Module.Context(Element.Text(f'> Message:\n**{message}**',Types.Text.KMD)))
 
         for cid, v in res_setting[rid].items():
             ch = await bot.client.fetch_public_channel(cid)
-
-            if 'secret' in repo_secret:
+            if 'secret' in repo_secret: 
                 secret = repo_secret['secret']
                 digest, signature = request.headers['X-HUB-SIGNATURE'].split("=", 1)
                 assert digest == "sha1", "Digest must be sha1"  # use a whitelist
                 h = hmac.HMAC(bytes(secret, "UTF8"), msg=body, digestmod=digest)
-
-
                 await ch.send(
                     ui.card_uni(icon.error, 'secret错误', f'repo:[{repo_name}]({repo_url})', is_highlight=False))
                 assert h.hexdigest() == signature, "Bad signature"
@@ -122,30 +119,24 @@ async def github_webhook(request: web.Request):
     return web.Response(body="Hi", status=200)
 
     # assert request.content_length < 1000000, "Request content too fat" # 1M
-
     # print("New commit by: {}".format((json.loads(body))['commits'][0]['author']['name']))
 
 
 def card_help():
     c = Card(color=ui.default_color)
-    # c.append(Module.Context('tips:完成操作后请点击底部按钮'))
-    c.append(Module.Header('第一步 配置URL'))
-    c.append(Module.Section(
-        '在您的仓库下 前往**setting** -> **Webhooks**界面 并在**Payload URL**处填写以下url：\n`https://api.kookbot.cn/hook`'))
-    c.append(Module.Header('第二步 Content type 选择 application/json'))
-    c.append(Module.Header('第三步 Secret处输入任意字母或留空'))
+    c.append(Module.Header("第一步 配置URL"))
+    c.append(Module.Section(Element.Text("在您的仓库下 前往**setting** -> **Webhooks**界面 并在**Payload URL**处填写以下url：\n`https://api.kookbot.cn/hook`",Types.Text.KMD)))
+    c.append(Module.Header("第二步 Content type 选择 application/json"))
+    c.append(Module.Header("第三步 Secret处输入任意字母或留空"))
     c.append(Module.Container(Element.Image('https://img.kookapp.cn/assets/2022-10/GVNhpJJnj91e60pp.png')))
-    c.append(Module.Header('第四步 点击页面底部Update Webhook'))
+    c.append(Module.Header("第四步 点击页面底部Update Webhook"))
     c.append(Module.Container(Element.Image('https://img.kookapp.cn/assets/2022-10/tj7t62gBsn1dp0pk.png')))
-    c.append(Module.Header('第五步 输入绑定指令'))
-    add = '\n若有密钥，请添加在id后方\n例：`g.bind xxx-xxx-xxx-xxx xxxxxx`'
-    c.append(Module.Section(
-        f'前往 **Recent Deliveries** 复制ping推送的id 返回开黑啦输入以下命令\n`g.bind {id}`\n例：`g.bind 5eb81820-4c93-11ed-96e9-87017811cb55`{add}'))
+    c.append(Module.Header("第五步 输入绑定指令"))
+    add = '\n若有密钥，请添加在id后方\n例：`g.bind xxx-xxx-xxx-xxx 密钥`'
+    c.append(Module.Section(Element.Text(
+        f"前往 **Recent Deliveries** 复制ping推送的id 返回开黑啦输入以下命令\n`g.bind {id}`\n例：`g.bind 5eb81820-4c93-11ed-96e9-87017811cb55`{add}",Types.Text.KMD)))
 
     c.append(Module.Container(Element.Image('https://img.kookapp.cn/assets/2022-10/PgZvhk66HF1dy0pm.png')))
-    # c.append(Module.ActionGroup(Element.Button('       我已完成上述操作      ',f'{"aid":"{aid}"},"action":"start"',theme=Types.Theme.SECONDARY)))
-    # global com_temp
-    # com_temp[aid] = 'start'
     return CardMessage(c)
 
 
@@ -239,12 +230,13 @@ async def bot1_add_uuid(msg: Message, d: str):
 #             return
 
 
+
+
+
 if __name__ == '__main__':
     app = web.Application()
     app.add_routes(routes)
+    # loop = asyncio.new_event_loop()
+    # timeout = aiohttp.ClientTimeout(total=10)
     asyncio.get_event_loop().run_until_complete(
-        asyncio.gather(web._run_app(app, host='0.0.0.0', port=14726), bot.start()))
-
-
-
-
+        asyncio.gather(web._run_app(app, host='127.0.0.1', port=5461), bot.start()))
